a25700d1e67d8dcbca0dec95300d77d8a9a1a9b5
karthikshankar2103 <62713456+karthikshankar2103@users.noreply.github.com>
2020-04-09 00:30:19 +0530

    Update OnePlus.java

--- OnePlus.java
+++ OnePlus.java
@@ -3,14 +3,6 @@ package git;
 
  class OnePlus {
 
-	public void version_1() {
-
-		System.out.println("version 1");
-	}
-	public void version_2() {
-
-		System.out.println("version 2");
-	}
 	
 	
 	public void version_3() {
